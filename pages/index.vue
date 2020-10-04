afbbc35394164073a41d03b7e01e608eedc89d4e
Mujahid Anuar <mujahidfanuar@gmail.com>
2020-10-04 22:19:39 +0800

    Make personality type filtering optional

--- pages/index.vue
+++ pages/index.vue
@@ -38,90 +38,160 @@
       >
       data on student enrollment in Malaysia's public universities in 2018.
     </p>
-    <section class="pb-20 md:px-32">
-      <div class="flex flex-row items-center justify-center pb-4">
-        <RoughNotation :is-show="true" type="highlight" color="#FFFEE2">
-          <p class="font-bold">Step 1:</p>
-        </RoughNotation>
-
-        <label for="course" class="pl-2 text-2xl text-center">
-          I'm studying...
-        </label>
-      </div>
-
-      <v-select
-        id="course"
-        v-model="selectedEduLevel"
-        :options="eduLevels"
-        placeholder="Select an education level."
-        class="text-gray-600"
+    <div class="mb-4">
+      <button
+        type="button"
+        class="inline-block px-3 py-2 text-sm leading-none text-gray-500 transition duration-300 ease-in-out transform rounded-lg hover:bg-indigo-100 hover:text-indigo-600 focus:outline-none hover:scale-105"
+        @click="isPersonalityTypeIncluded = !isPersonalityTypeIncluded"
       >
-        <p slot="no-options">
-          Sorry, we couldn't find your education level.
-        </p>
-      </v-select>
-    </section>
+        {{
+          isPersonalityTypeIncluded === false
+            ? 'Try using our Personality Type filter!'
+            : 'Go to default filtering'
+        }}
+      </button>
+    </div>
 
-    <section class="pb-20 md:px-32">
-      <div class="flex flex-row items-center justify-center pb-4">
-        <RoughNotation :is-show="true" type="highlight" color="#FFFEE2">
-          <p class="font-bold">Step 2:</p>
-        </RoughNotation>
+    <div v-if="isPersonalityTypeIncluded === false">
+      <section class="pb-20 md:px-32">
+        <div class="flex flex-row items-center justify-center pb-4">
+          <RoughNotation :is-show="true" type="highlight" color="#FFFEE2">
+            <p class="font-bold">Step 1:</p>
+          </RoughNotation>
 
-        <label for="job" class="pl-2 text-2xl text-center">
-          My personality type is...<br />
-          <p class="text-sm text-gray-600">
-            *based on the
-            <a
-              href="https://www.16personalities.com/free-personality-test"
-              target="_blank"
-              rel="noopener noreferrer"
-              class="text-indigo-700 underline hover:text-green-700"
-            >
-              Myers-Briggs Type Indicator</a
-            >
+          <label for="course" class="pl-2 text-2xl text-center">
+            I'm studying...
+          </label>
+        </div>
+
+        <v-select
+          id="course"
+          v-model="selectedEduLevel"
+          :options="eduLevels"
+          placeholder="Select an education level."
+          class="text-gray-600"
+        >
+          <p slot="no-options">
+            Sorry, we couldn't find your education level.
           </p>
-        </label>
-      </div>
-      <v-select
-        id="job"
-        v-model="selectedPersonalityType"
-        :options="personalityTypes"
-        :disabled="isEduLevelSelected === false"
-        placeholder="Select a personality type."
-        class="text-gray-600"
-      >
-        <div slot="no-options">
-          No matching personality type found.
-        </div>
-      </v-select>
-    </section>
+        </v-select>
+      </section>
 
-    <section class="pb-20 md:px-32">
-      <div class="flex flex-row items-center justify-center pb-4">
-        <RoughNotation :is-show="true" type="highlight" color="#FFFEE2">
-          <p class="font-bold">Step 3:</p>
-        </RoughNotation>
+      <section class="pb-20 md:px-32">
+        <div class="flex flex-row items-center justify-center pb-4">
+          <RoughNotation :is-show="true" type="highlight" color="#FFFEE2">
+            <p class="font-bold">Step 2:</p>
+          </RoughNotation>
 
-        <label for="job" class="pl-2 text-2xl text-center">
-          You are compatible to work in...
-        </label>
-      </div>
-      <v-select
-        id="job"
-        v-model="selectedIndustry"
-        :options="industries"
-        :disabled="
-          isEduLevelSelected === false && isPersonalityTypeSelected === false
-        "
-        placeholder="Select an industry."
-        class="text-gray-600"
-      >
-        <div slot="no-options">
-          No job industry is found for your selected education level.
+          <label for="job" class="pl-2 text-2xl text-center">
+            I want to work in...
+          </label>
         </div>
-      </v-select>
-    </section>
+        <v-select
+          id="job"
+          v-model="selectedIndustry"
+          :options="eduLevelIndustries"
+          :disabled="isEduLevelSelected === false"
+          placeholder="Select an industry."
+          class="text-gray-600"
+        >
+          <div slot="no-options">
+            No job industry is found for your selected education level.
+          </div>
+        </v-select>
+      </section>
+    </div>
+
+    <div v-else>
+      <!-- Education level -->
+      <section class="pb-20 md:px-32">
+        <div class="flex flex-row items-center justify-center pb-4">
+          <RoughNotation :is-show="true" type="highlight" color="#FFFEE2">
+            <p class="font-bold">Step 1:</p>
+          </RoughNotation>
+
+          <label for="course" class="pl-2 text-2xl text-center">
+            I'm studying...
+          </label>
+        </div>
+
+        <v-select
+          id="course"
+          v-model="selectedEduLevel"
+          :options="eduLevels"
+          placeholder="Select an education level."
+          class="text-gray-600"
+        >
+          <p slot="no-options">
+            Sorry, we couldn't find your education level.
+          </p>
+        </v-select>
+      </section>
+
+      <!-- Personality type -->
+      <section class="pb-20 md:px-32">
+        <div class="flex flex-row items-center justify-center pb-4">
+          <RoughNotation :is-show="true" type="highlight" color="#FFFEE2">
+            <p class="font-bold">Step 2:</p>
+          </RoughNotation>
+
+          <label for="job" class="pl-2 text-2xl text-center">
+            My personality type is...<br />
+            <p class="text-sm text-gray-600">
+              *based on the
+              <a
+                href="https://www.16personalities.com/free-personality-test"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="text-indigo-700 underline hover:text-green-700"
+              >
+                Myers-Briggs Type Indicator</a
+              >
+            </p>
+          </label>
+        </div>
+        <v-select
+          id="job"
+          v-model="selectedPersonalityType"
+          :options="personalityTypes"
+          :disabled="isEduLevelSelected === false"
+          placeholder="Select a personality type."
+          class="text-gray-600"
+        >
+          <div slot="no-options">
+            No matching personality type found.
+          </div>
+        </v-select>
+      </section>
+
+      <!-- Industry -->
+      <section class="pb-20 md:px-32">
+        <div class="flex flex-row items-center justify-center pb-4">
+          <RoughNotation :is-show="true" type="highlight" color="#FFFEE2">
+            <p class="font-bold">Step 3:</p>
+          </RoughNotation>
+
+          <label for="job" class="pl-2 text-2xl text-center">
+            You are compatible to work in...
+          </label>
+        </div>
+        <v-select
+          id="job"
+          v-model="selectedIndustry"
+          :options="personalityIndustries"
+          :disabled="
+            isEduLevelSelected === false && isPersonalityTypeSelected === false
+          "
+          placeholder="Select an industry."
+          class="text-gray-600"
+        >
+          <div slot="no-options">
+            No job industry is found for your selected education level.
+          </div>
+        </v-select>
+      </section>
+    </div>
+
     <button
       class="py-2 mb-12 font-bold text-white bg-blue-400 rounded sm:self-center sm:w-56 focus:outline-none"
       :class="{
@@ -145,12 +215,13 @@
 <script lang="ts">
 import Vue from 'vue'
 import 'vue-select/dist/vue-select.css'
-// import { eduLevelToIndustry } from '@/data/eduLevelToIndustry.ts'
+import { eduLevelToIndustry } from '@/data/eduLevelToIndustry.ts'
 import { personalityToIndustry } from '@/data/personalityToIndustry.ts'
 
 export default Vue.extend({
   data() {
     return {
+      isPersonalityTypeIncluded: false,
       eduLevels: [
         'No Tertiary Education',
         'Professional Certificate',
@@ -194,8 +265,11 @@ export default Vue.extend({
         this.selectedPersonalityType !== null
       )
     },
-    industries(): string[] {
+    personalityIndustries(): string[] {
       return personalityToIndustry[this.selectedPersonalityType]
+    },
+    eduLevelIndustries(): string[] {
+      return eduLevelToIndustry[this.selectedEduLevel]
     },
     isOptionsFilledIn(): Boolean {
       return (
