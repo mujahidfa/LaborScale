d17942c713b746d0371c4c108b1d07f3061a668c
Mujahid Anuar <mujahidfanuar@gmail.com>
2020-10-04 18:41:01 +0800

    Update about page

--- pages/about.vue
+++ pages/about.vue
@@ -12,18 +12,6 @@
       </RoughNotation>
     </h2>
     <p class="pb-8 text-gray-700 md:text-md">
-      This project is conducted for the
-      <a
-        href="https://www.ilmia.gov.my/lmiopendatachallenge/"
-        target="_blank"
-        rel="noopener noreferrer"
-        class="text-indigo-700 underline hover:text-green-700"
-      >
-        Labour Market Information (LMI) Open Data Challenge</a
-      >
-      by <span class="font-semibold">Groupaq Weebs</span>. Groupaq Weebs team
-      members are Ikmal Syafiq Bin Mahadi, Khairina Binti Ibrahim and Mujahid
-      bin Fakhrul Anuar.
       <a
         href="https://twitter.com/LaborScale"
         target="_blank"
@@ -46,16 +34,27 @@
       </RoughNotation>
     </h2>
     <p class="pb-8 text-gray-700 md:text-md">
-      This application is build based on the data provided by ILMIA for the
+      This project was made by Ikmal Syafiq Bin Mahadi, Khairina Binti Ibrahim
+      and Mujahid bin Fakhrul Anuar. The data for the labor demand and student
+      supply is based on 2020 data scraped from
       <a
-        href="https://www.ilmia.gov.my/lmiopendatachallenge/"
+        href="https://www.jobstreet.com.my/"
         target="_blank"
         rel="noopener noreferrer"
         class="text-indigo-700 underline hover:text-green-700"
       >
-        Labour Market Information (LMI) Open Data Challenge</a
+        JobStreet</a
       >
-      specifically on the job vacancy and student university output. This
+      from January 2020 to June 2020 & data from
+      <a
+        href="http://www.data.gov.my/"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="text-indigo-700 underline hover:text-green-700"
+      >
+        MAMPU's Public Sector Open Data Portal</a
+      >
+      on student enrollment in Malaysia's public universities in 2018. This
       application is built using Python,
       <a
         href="https://vuejs.org/"
@@ -83,7 +82,8 @@
     </h2>
     <p class="pb-8 text-gray-700 md:text-md">
       What are the courses that the students should take that are in demand in
-      the job market and offer the highest salary?
+      the job market and offer the highest salary? What courses are suitable for
+      students based on their personality type?
     </p>
 
     <h2 class="pb-4 text-xl font-semibold text-gray-800 md:text-2xl">
@@ -93,16 +93,19 @@
     </h2>
     <ol class="pb-8 text-gray-700 list-decimal list-inside md:text-md">
       <li>
+        To match students with suitable courses based on personality type
+      </li>
+      <li>
         To find the student output to job ratio on the user selected course
       </li>
       <li>To find the average wage on the user selected course</li>
       <li>To find male to female ratio on the field in the selected course</li>
       <li>
-        To connect the students with resources through Linked In in the selected
+        To connect the students with resources through LinkedIn in the selected
         course
       </li>
       <li>
-        To identify the top five student to job ration across the industry
+        To identify the top five student to job ratio across the industry
       </li>
       <li>To identify the top five industry with the most job opening</li>
       <li>To identify the top five students output for the workforce</li>
